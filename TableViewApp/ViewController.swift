9d429aa35378c0acf8c3326b373cdd303b8e77eb
kirifu_mini <uniuniunu2525@gmail.com>
2020-10-21 15:44:42 +0900

    first commit

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by Takahiro Kirifu on 2020/10/21.
+//  Copyright © 2020 Takahiro Kirifu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
